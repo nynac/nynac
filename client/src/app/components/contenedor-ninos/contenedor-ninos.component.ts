3365f7235408f1bb620e2c51b15aff3fe0b527e6
Cristian Hernandez <cristian.hernandez@tectijuana.edu.mx>
2019-08-19 13:56:42 -0700

    Se subió para que vic me copie mis componentes

--- client/src/app/components/contenedor-ninos/contenedor-ninos.component.ts
+++ client/src/app/components/contenedor-ninos/contenedor-ninos.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'contenedor-ninos',
-  templateUrl: './contenedor-ninos.component.html',
-  styleUrls: ['./contenedor-ninos.component.css']
+	selector: 'contenedor-ninos',
+	templateUrl: './contenedor-ninos.component.html',
+	styleUrls: ['./contenedor-ninos.component.css']
 })
 export class ContenedorNinosComponent implements OnInit {
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit() {
-  }
-
+	ngOnInit() {
+	}
 }
+
