e3fc3ffdf85c88613018c5f37c3ceab91fa45c11
victor15210552 <chanchezcal@gmail.com>
2019-08-20 15:33:31 -0700

    Estructura DM_ninos

---
+++ client/src/app/components/dm-ninos/dm-ninos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'dm-ninos',
+  templateUrl: './dm-ninos.component.html',
+  styleUrls: ['./dm-ninos.component.css']
+})
+export class DmNinosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
