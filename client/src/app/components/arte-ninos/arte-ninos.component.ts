9bbd7776f8ba90eb89c1e1e100554954d677e90b
Cristian Hernandez <cristian.hernandez@tectijuana.edu.mx>
2019-08-21 10:53:51 -0700

    Se completó el diseño de ARTE niños

---
+++ client/src/app/components/arte-ninos/arte-ninos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'arte-ninos',
+  templateUrl: './arte-ninos.component.html',
+  styleUrls: ['./arte-ninos.component.css']
+})
+export class ArteNinosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
