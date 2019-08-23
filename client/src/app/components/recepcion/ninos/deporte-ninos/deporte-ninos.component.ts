315c700fa50a7ebe6b439ae5de2a504a374890e8
victor15210552 <chanchezcal@gmail.com>
2019-08-22 21:46:23 -0700

    Estructura Deporte ninos

---
+++ client/src/app/components/recepcion/ninos/deporte-ninos/deporte-ninos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'deporte-ninos',
+  templateUrl: './deporte-ninos.component.html',
+  styleUrls: ['./deporte-ninos.component.css']
+})
+export class DeporteNinosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
