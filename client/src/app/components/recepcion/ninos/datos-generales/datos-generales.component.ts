f21b964dcd64454e205a5be304b750dffe669234
Cristian Hernandez <cristian.hernandez@tectijuana.edu.mx>
2019-09-05 14:37:51 -0700

    ya hace busqueda y muestra los valiores en el DOM

--- client/src/app/components/recepcion/ninos/datos-generales/datos-generales.component.ts
+++ client/src/app/components/recepcion/ninos/datos-generales/datos-generales.component.ts
@@ -7,61 +7,96 @@ import { RegistrationService } from '../../../../registration.service';
 	templateUrl: './datos-generales.component.html',
 	styleUrls: ['./datos-generales.component.css']
 })
-export class DatosGeneralesComponent implements OnInit {
+export class DatosGeneralesComponent {
+	opcion: string = '';
 	miembros : any;
 	datos_miembro : any;
-	nuevo_id_miembro : any;
+	nuevo_id_miembro : string = '';
 	url = "https://api-remota.conveyor.cloud/api/";
 
-	constructor(private http : HttpClient) { }
+	//Resultados de busqueda
+	resultado : any = "";
 	
 
-	ngOnInit() {
-		var response = this.http.get(this.url + "/miembro");
-		response.subscribe((data)=>this.miembros = data);
+	constructor(private http : HttpClient) { }
+
+	radioChange (event: any){
+		this.opcion = event.target.value;
+
+		if (this.opcion == "nuevo")
+			this.get_nuevo_miembro();
 	}
 
-	guardar_DG(formData) {
-		this.nuevo_id_miembro = (this.miembros[this.miembros.length -1].miembroID);
+	//EVENTO DEL FORMULARIO DE DATOS GENERALES
+	guardar_DG(datos_formulario) {
+		if (this.opcion == "nuevo")
+			this.nuevo(datos_formulario);
+		else
+			this.modificar(datos_formulario);
+	}
 
-		//GUARDAR MIEMBRO
-		/*this.datos_miembro = {
+	//Obtener nuevo miembro MÉTODO AUXILIAR
+	get_nuevo_miembro(){
+		var response = this.http.get(this.url + "/miembro");
+		response.subscribe((data)=> { 
+			this.miembros = data;
+			//Asignamos el id a nuevo_id_miembro porque será utilizada en el DOM
+			this.nuevo_id_miembro =  (this.miembros[this.miembros.length -1].miembroID + 1); 
+		},
+		error =>{
+			console.log("Error", error)
+		});
+	}
+
+	nuevo(datos_formulario){
+		//1. Recalcula el número de miembro, en dado caso que ya hayan registrado uno mientras estaba en proceso
+		this.get_nuevo_miembro()
+		
+		//2. Guardamos al niño en la tabla miembros
+		this.datos_miembro = {
+			miembroID : this.nuevo_id_miembro,
 			tipo : "niño",
-			estado : "true",
-			miembroID : this.nuevo_id_miembro
+			estado : "true"
 		}
 
-		this.http.post(this.url + 'miembro', this.datos_miembro).toPromise().then((data:any) => {
-			alert("Se ha insertado el miembro");
-		});*/
-
-		//GUARDAR DATOS GENERALES MIEMBRO
-		formData['miembroID'] = this.nuevo_id_miembro;
-		formData['idNinosDG'] = this.nuevo_id_miembro;
-/*
-		this.http.post(this.url + "Nino_DG", formData).subscribe(data  => {
-			console.log("POST Request is successful ", data);
+		this.http.post(this.url + 'miembro', this.datos_miembro).subscribe(data  => {
+			console.log("Se guardó el niño como nuevo miembro.", data);
 		},
 		error  => {
-			console.log("Error", error);
+			console.log("Error al guardar en la tabla miembro", error);
 		});
-		*/
 
+		//3. Guardamos al niño en la tabla ninos_DG
+		datos_formulario['miembroID'] = this.nuevo_id_miembro;
+		datos_formulario['idNinosDG'] = this.nuevo_id_miembro;
+
+		this.http.post(this.url + "Nino_DG", datos_formulario).subscribe(data  => {
+			console.log("Se guardaron los datos generales.", data);
+		},
+		error  => {
+			console.log("Error al guardar datos generales.", error);
+		});
+	}
+
+	//TODO PARA MODIFICAR UN MIEMBRO DE TIPO NIÑO
+	ng_busq_Form(form_buscar){
+		var response = this.http.get(this.url + "/miembro/" + form_buscar['miembroID']);
+		response.subscribe((data : any[])=> { 
+			this.resultado = data;
+		},
+		error =>{
+			console.log("Error", error)
+		});
+	}
+
+	modificar(datos_formulario){
+		console.log(datos_formulario);
 		//GUARDAR DATOS GENERALES MIEMBRO
-		this.http.put(this.url + "Nino_DG/1", formData).subscribe(data  => {
+		/*this.http.put(this.url + "Nino_DG/1", datos_formulario).subscribe(data  => {
 			console.log("Se han modificado los valores correctamente", data);
 		},
 		error  => {
 			console.log("Error", error);
-		});
+		});*/
 	}
-
-	//Opciones agregar o modificar
-	nuevo(obj){
-		alert("Se agregará un nuevo miembro");
-	}
-	modificar(obj){
-		alert("Semodificará un niño");
-	}
-
 }
